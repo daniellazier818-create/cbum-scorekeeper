d21c209b155a31c5ba872fec083b9ce1f50a2f27
daniellazier818-create <daniellazier818@gmail.com>
2026-08-26 17:59:12 -0700

    Cache live sync client for offline v2.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cbum-cup-v2-4-pages-icons';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='cbum-cup-v2-5-live';
+const ASSETS=['./','./index.html','./styles.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
