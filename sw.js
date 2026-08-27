6c2fddfa7d3c9f36d4c18830efd6f080dad5c0a4
daniellazier818-create <daniellazier818@gmail.com>
2026-08-27 13:32:58 -0700

    Cache standings layout update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cbum-cup-v3-2-granular-reset-2';
-const ASSETS=['./','./index.html','./styles.css','./heritage.css','./ux.css','./v3patch.css','./live_detail.css','./reset_match.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./ux.js','./v3patch.js','./live_detail.js','./reset_match.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='cbum-cup-v3-2-1-even-columns';
+const ASSETS=['./','./index.html','./styles.css','./heritage.css','./ux.css','./v3patch.css','./live_detail.css','./reset_match.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./standings_layout.js','./ux.js','./v3patch.js','./live_detail.js','./reset_match.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
