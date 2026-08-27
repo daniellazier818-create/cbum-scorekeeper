bd55faadf9db3165bf144e180e13986b23fe168f
daniellazier818-create <daniellazier818@gmail.com>
2026-08-27 08:15:36 -0700

    Refresh offline cache for v2.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cbum-cup-v2-5-live';
+const CACHE='cbum-cup-v2-6-formats';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
