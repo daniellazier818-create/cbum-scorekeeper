140db202a1564e2586fa667940149591676072a0
daniellazier818-create <daniellazier818@gmail.com>
2026-08-27 15:11:33 -0700

    Cache v3.4 peach branding assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cbum-cup-v3-3-dynamic-summary-tartan-r2';
-const ASSETS=['./','./index.html','./styles.css','./heritage.css','./ux.css','./v3patch.css','./live_detail.css','./reset_match.css','./v33.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./standings_layout.js','./ux.js','./rounds_summary.js','./v3patch.js','./live_detail.js','./reset_match.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
+const CACHE='cbum-cup-v3-4-peach-branding';
+const ASSETS=['./','./index.html','./styles.css','./heritage.css','./ux.css','./v3patch.css','./live_detail.css','./reset_match.css','./v33.css','./v34.css','./data.js','./core1.js','./core2.js','./core3.js','./render1.js','./render2.js','./render3.js','./standings_layout.js','./ux.js','./rounds_summary.js','./v3patch.js','./live_detail.js','./reset_match.js','./live.js','./bind.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
