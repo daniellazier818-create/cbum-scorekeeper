006e93dbcce90f55ae24c0425b6e3a92063df125
daniellazier818-create <daniellazier818@gmail.com>
2026-08-31 16:49:56 -0700

    Simplify Travel routing fix

--- travel_fix.js
+++ travel_fix.js
@@ -1,4 +1,4 @@
-/* v3.7.1 travel routing + day selector fix */
+/* v3.7.1 travel routing fix */
 const renderBeforeTravelFix=render;
 render=function(){
   if(state.ui.tab!=='travel')return renderBeforeTravelFix();
@@ -8,13 +8,3 @@ render=function(){
   bind();
   if(typeof applyLiveMode==='function')applyLiveMode();
 };
-
-document.addEventListener('click',e=>{
-  const day=e.target.closest?.('[data-travel-day]');
-  if(!day)return;
-  state.ui.travelDay=day.dataset.travelDay;
-  save();
-  render();
-  const picker=document.querySelector('.travel-day-picker');
-  if(picker)picker.scrollIntoView({block:'nearest'});
-});
