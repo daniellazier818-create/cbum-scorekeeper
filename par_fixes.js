5a76e6661183fd51d6bb1d0a1ec16a0cdbdc9cd0
daniellazier818-create <daniellazier818@gmail.com>
2026-09-07 08:02:29 -0700

    Add official Elie women par and stroke indexes

--- par_fixes.js
+++ par_fixes.js
@@ -1,5 +1,18 @@
-/* Gender-specific par corrections for tees where women's par differs from men's par. */
+/* Gender-specific hole-data corrections where women's par/SI differs from men's. */
 (function(){
+  const elie=COURSE_DATA.elie.tees;
+  const elieWomenParLong=[5,4,3,4,4,4,4,4,5,4,3,5,4,4,4,4,5,4];
+  const elieWomenParGreen=[5,4,3,4,4,4,3,4,5,4,3,5,4,4,4,4,5,4];
+  const elieWomenSILong=[3,9,13,1,15,7,17,5,11,18,6,16,8,2,12,4,14,10];
+  const elieWomenSIGreen=[11,7,17,1,15,9,13,3,6,16,18,5,2,8,14,4,12,10];
+
+  elie.White.womenPar=[...elieWomenParLong];
+  elie.Yellow.womenPar=[...elieWomenParLong];
+  elie.Green.womenPar=[...elieWomenParGreen];
+  elie.White.womenSI=[...elieWomenSILong];
+  elie.Yellow.womenSI=[...elieWomenSILong];
+  elie.Green.womenSI=[...elieWomenSIGreen];
+
   const nb=COURSE_DATA.northberwick.tees;
 
   // North Berwick Blue is par 71 for men, par 74 for women.
