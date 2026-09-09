82a55ae7ef1f69e02601a62fc805c272bf6888d0
daniellazier818-create <daniellazier818@gmail.com>
2026-09-09 04:48:29 -0700

    Rename Castle Stuart combo tee to Green Red combo

--- castle_stuart_combo.js
+++ castle_stuart_combo.js
@@ -1,7 +1,10 @@
-/* Castle Stuart Blue / Combo tee for lasses */
-COURSE_DATA.castlestuart.tees['Blue / Combo']={
+/* Castle Stuart Green/Red combination tee for Emily. */
+const CASTLE_STUART_GREEN_RED_COMBO={
   yards:[330,486,266,161,332,411,359,192,330,341,115,504,303,347,314,310,133,490],
   par:[4,5,4,3,4,5,4,3,4,4,3,5,4,4,4,4,3,5],
   si:[9,5,13,17,7,3,1,15,11,14,16,2,4,10,8,18,6,12],
-  women:[74.4,135,72]
+  women:[74.4,134,72]
 };
+COURSE_DATA.castlestuart.tees['Green/Red Combo']=CASTLE_STUART_GREEN_RED_COMBO;
+/* Backward compatibility for devices that previously stored the old internal name. */
+Object.defineProperty(COURSE_DATA.castlestuart.tees,'Blue / Combo',{value:CASTLE_STUART_GREEN_RED_COMBO,enumerable:false,configurable:true});
